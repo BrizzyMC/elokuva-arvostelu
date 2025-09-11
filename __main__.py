99a2c8bc014e91c1fed892b3a6e4b6a0cd932e03
Viljam Vänskä <vili123.vanska@gmail.com>
2025-09-11 13:35:24 +0300

    Create __main__.py
    luo uusi tyhjä main

---
+++ __main__.py
@@ -0,0 +1,17 @@
+"""
+=================================================================
+
+Nimi:       __main__.py
+kuvaus:     Tämä tiedosto pitää sisällään ohjelman "main loopin",
+            tähän silmukkaan on kasattu ohjelman koodi toimivaksi
+            kokonaisuudeksi. Tämän tiedoston suorittaminen
+            käynnistää ohjelman.
+
+Tekiä:      Viljam Vänskä
+Päivämäärä: 11.9.2025
+Versio:     1.0
+
+=================================================================
+"""
+
+pass
